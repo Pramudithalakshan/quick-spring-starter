838347dac3d72dbf4c248a21394589d18db599c0
Pramudithalakshan <pramudithalakshan00@gmail.com>
2026-04-02 22:00:08 +0530

    feat: implement security filter chain and jwt decoder

---
+++ src/main/java/org/dreamdevzone/quickspringstarter/AutoConfiguration.java
@@ -0,0 +1,39 @@
+package org.dreamdevzone.quickspringstarter;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+@EnableConfigurationProperties(QuickSecurityProperties.class)
+public class AutoConfiguration {
+    @Bean
+    @ConditionalOnMissingBean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, QuickSecurityProperties properties) {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+    @Bean
+    @ConditionalOnMissingBean
+    public JwtDecoder jwtDecoder(QuickSecurityProperties properties) {
+        String secret = properties.getJwtSecret();
+        if(secret == null ||  secret.isBlank()) {
+            secret = java.util.UUID.randomUUID().toString();
+        }
+        SecretKey spec = new SecretKeySpec(secret.getBytes(), "HmacSHA256");
+        return NimbusJwtDecoder.withSecretKey(spec).build();
+    }
+}
